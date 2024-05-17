22b962dacfe75f30c996b71d61d573e22e667094
Sho Miyamoto <me@shqld.dev>
2024-05-17 17:00:33 +0900

    Update .fzf

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,7 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+eval "$(fzf --zsh)"
